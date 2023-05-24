5a77f159a37d064201b6e3e68f81f8a6ef44714b
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2023-05-24 09:55:54 +0300

    Add coroutines ftp example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "util.h"
 #include "FtpExampleSync.h"
 #include "FtpExampleAsync.h"
+#include "FtpExampleCoro.h"
+#include "SyncWaitTask.h"
 #include <thread>
 
 void useSync(const std::string& path, const std::string& pattern)
@@ -40,12 +42,34 @@ void useAsync(const std::string& path, const std::string& pattern)
     }
 }
 
+void useCoro(const std::string& path, const std::string& pattern)
+{
+    LogInfo("======== Using coroutines");
+    try
+    {
+	    kw::FTPExampleCoro ftp;
+	    auto fileTask = ftp.downloadFirstMatch(path,
+			[&pattern](std::string_view f) { return f.ends_with(pattern); },
+	        [](int progress) { LogInfo("Download: %d%%", progress); });
+        auto waitTask = startTask(std::move(fileTask));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        LogInfo("Doing my job...");
+        std::string file = waitTask.result();
+        LogInfo("downloadFirstMatch success: %s", file.c_str());
+    }
+    catch (const std::exception& e)
+    {
+        LogError("downloadFirstMatch failed: %s", e.what());
+    }
+}
+
 int main(int, char**)
 {
     const std::string path = getProjectPath() + "/src/include";
     const std::string pattern = ".h";
     useSync(path, pattern);
     useAsync(path, pattern);
+    useCoro(path, pattern);
 
     return 0;
 }
